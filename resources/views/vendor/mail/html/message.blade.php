fcb772acc22b497dd2dd25a8bd8f0c54e027360c
unknown <henriquehps1997@gmail.com>
2020-09-22 14:51:18 -0300

    correcao na url de imagem mail

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -1,7 +1,7 @@
 @component('mail::layout')
 {{-- Header --}}
 @slot('header')
-@component('mail::header', ['url' => config('app.url')])
+@component('mail::header', ['url' => 'https://iesb.br/'])
 <p>Canal Direto</p>
 @endcomponent
 @endslot
