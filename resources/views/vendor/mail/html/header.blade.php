f32460ca793ea5063b6487022c8ae55e737a0ff3
marcosmpob <marcosmpob@gmail.com>
2020-09-22 14:47:47 -0300

    ajusteImagemLogo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="http://apps.iesb.br/static/logos/logo-iesb-64px.png" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
+<img src="http://apps.iesb.br/static/logos/logo-iesb-64px.png">
 </a>
 </td>
 </tr>
