0a3783148c01c774a5218209d4f8dbd97df378c8
unknown <henriquehps1997@gmail.com>
2020-10-22 17:00:23 -0300

    correcao no seeder de usuarios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(StatusTicketsSeeder::class);
         $this->call(MenuSeeder::class);
         $this->call(SubMenuSeeder::class);
         $this->call(SistemasSeeder::class);
         $this->call(PapeisSeeder::class);
+        $this->call(PapeisUsuarioSeeder::class);
+        $this->call(PapeisMenuSeeder::class);
         // $this->call(PapeisSeeder::class);
     }
 }
